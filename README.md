713b42a7b31be47dc9e6071fd582d73cee8061b1
Arpit Gupta <glex.qsd@gmail.com/agupta80@gatech.edu>
2013-07-09 14:36:23 -0400

    Automated the Mininet setup

--- README.md
+++ README.md
@@ -5,4 +5,27 @@ SDX Platform
 How to run it?
 --------------
 
-Execute `python pyretic.py pyretic.sdx.platform`. Then, launch Mininet
+Execute `python pyretic.py pyretic.sdx.platform`. 
+
+Launch Mininet:
+Transfer the file 'sdx_mininet.py' to your Mininet VM.
+Run the command, ' sudo python sdx_mininet.py' 
+This Mininet script creates a single topology with three hosts.  It does all the interface configurations, routing table updates etc. and finally runs two ping tests. 
+
+If any problem with this script then follow these steps manually:
+
+Launching Mininet
+'sudo mn --controller=remote --topo=single,3 --mac --arp'
+
+Configuring the hosts in Mininet
+'h1 ifconfig lo:40 110.0.0.1 netmask 255.255.255.0 up'
+'h2 ifconfig lo:40 120.0.0.1 netmask 255.255.255.0 up'
+'h3 ifconfig lo:40 130.0.0.1 netmask 255.255.255.0 up'
+
+'h1 route add -net 130.0.0.0 netmask 255.255.255.0 gw 10.0.0.2 h1-eth0'
+'h3 route add -net 110.0.0.0 netmask 255.255.255.0 gw 10.0.0.2 h3-eth0'
+
+Starting a ping with the appropriate source and destination mac addresses
+'h1 ping -I 110.0.0.1 130.0.0.1'
+'h3 ping -I 130.0.0.1 110.0.0.1'
+
---
+++ sdx_mininet.py
@@ -0,0 +1,69 @@
+## Author: Arpit Gupta (glex.qsd@gmail.com)
+"""
+    scp this script to VM running Mininet. 
+    Make sure you have appropriate $PYTHONPATH
+    Make sure your controller ip address is 192.168.56.1, else modify the controller_ip
+        
+"""
+
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import Controller, RemoteController
+
+
+
+controller_ip='192.168.56.1'
+
+
+class SDXSwitchTopo(Topo):
+    "SDX: Single switch connected to n hosts."
+    def __init__(self, n=2, **opts):
+        # Initialize topology and default options
+        Topo.__init__(self, **opts)
+        switch = self.addSwitch('s1')
+        # Python's range(N) generates 0..N-1
+        for h in range(n):
+            host = self.addHost('h%s' % (h + 1))
+            self.addLink(host, switch)
+
+def Traffic_Offloading():
+    "Create and test SDX Traffic Offloading Module"
+    topo = SDXSwitchTopo(n=3)
+    c0 = RemoteController( 'c0', ip=controller_ip )
+    net = Mininet(topo, autoSetMacs=True, autoStaticArp=True)
+    net.controllers=[c0]
+    net.start()
+    hosts=net.hosts
+    print hosts
+    for host in hosts:
+        if host.name=='h1':
+	    print "h1 detected"
+	    host.cmd('ifconfig lo:40 110.0.0.1 netmask 255.255.255.0 up')
+	    host.cmd('route add -net 130.0.0.0 netmask 255.255.255.0 gw 10.0.0.2 h1-eth0')
+	if host.name=='h2':
+	    host.cmd('ifconfig lo:40 120.0.0.1 netmask 255.255.255.0 up')
+	if host.name=='h3':
+            host.cmd('ifconfig lo:40 130.0.0.1 netmask 255.255.255.0 up') 
+	    host.cmd('route add -net 110.0.0.0 netmask 255.255.255.0 gw 10.0.0.2 h3-eth0')
+    """print "Showing the inteface updates"
+    
+    for host in hosts:
+	print host.cmd('ifconfig')  
+    """
+    for host in hosts:
+	if host.name=='h1':
+            host.cmdPrint('ping -c 5 -I 110.0.0.1 130.0.0.1')
+        if host.name=='h3':
+            host.cmdPrint('ping -c 5 -I 130.0.0.1 110.0.0.1')
+
+    net.stop()
+
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    # Currently we are running mininet to test traffic offloading module for SDX
+    # Infuture we can write various other test modules 
+    Traffic_Offloading()
